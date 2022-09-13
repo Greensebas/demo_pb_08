344326f5963e52dcab8111f6db0fffba55bee139
Greensebas <greensebas@hotmail.com>
2022-09-12 21:16:11 -0300

    arreglado

---
+++ middlewares/authorizer.js
@@ -0,0 +1,15 @@
+const {users} = require("../data/data")
+
+const authorizer = (req, res, next) => {
+    const {id} = req.query;
+    const user = users.find(user => user.id === +id);
+    if(user) {
+        req.user = user;
+        next();
+    }
+    else {
+        res.status(401).send()
+    }
+}
+
+module.exports = authorizer
